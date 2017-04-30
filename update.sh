259f3a7a697874f179fbe4d49bed5f9419590c06
Yves Riedener <git@reecube.com>
2017-04-30 15:56:13 +0200

    fixed update script

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ php ../composer/composer.phar install
 
 php bin/console doctrine:schema:update --force
 php bin/console cache:clear --env=prod
-php bin/console assetic:dump --env=prod --no-debug
+php bin/console assets:install --env=prod
