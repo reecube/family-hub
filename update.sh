a7d12aa3994902255c04559bb8975de91fb327d9
Yves Riedener <git@reecube.com>
2017-05-10 00:07:32 +0200

    use symlinks for assets

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ git pull
 php ../composer/composer.phar install
 
 php bin/console cache:clear --env=prod
-php bin/console assets:install --env=prod
+php bin/console assets:install --symlink --env=prod
 php bin/console assetic:dump
