c0089933ec984d4a8b7cabf2f8e53e0edb537970
Yves Riedener <git@reecube.com>
2017-05-12 12:14:53 +0200

    formatted scss/js files after editorconfig change

--- src/AppBundle/Resources/public/js/base.js
+++ src/AppBundle/Resources/public/js/base.js
@@ -1,47 +1,47 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    if (!location || !location.search || !location.pathname || !history || !history.pushState) {
-        return;
+  if (!location || !location.search || !location.pathname || !history || !history.pushState) {
+    return;
+  }
+
+  var parseQuery = function (qstr) {
+    var query = {};
+    var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
+    for (var i = 0; i < a.length; i++) {
+      var b = a[i].split('=');
+      query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+    return query;
+  };
+
+  var stringifyQuery = function (query) {
+    if (!query || !query.length) {
+      return '';
     }
 
-    var parseQuery = function (qstr) {
-        var query = {};
-        var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
-        for (var i = 0; i < a.length; i++) {
-            var b = a[i].split('=');
-            query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
-        }
-        return query;
-    };
+    var qstr = '?';
 
-    var stringifyQuery = function (query) {
-        if (!query || !query.length) {
-            return '';
-        }
+    for (var i in query) {
+      if (query.hasOwnProperty(i)) {
+        qstr += encodeURIComponent(i) + '=' + encodeURIComponent(query[i]);
+      }
+    }
 
-        var qstr = '?';
+    return qstr;
+  };
 
-        for (var i in query) {
-            if (query.hasOwnProperty(i)) {
-                qstr += encodeURIComponent(i) + '=' + encodeURIComponent(query[i]);
-            }
-        }
+  var deleteMessagesAndErrors = function (query) {
+    if (query.messages) delete query.messages;
+    if (query.message) delete query.message;
+    if (query.errors) delete query.errors;
+    if (query.error) delete query.error;
 
-        return qstr;
-    };
+    return query;
+  };
 
-    var deleteMessagesAndErrors = function (query) {
-        if (query.messages) delete query.messages;
-        if (query.message) delete query.message;
-        if (query.errors) delete query.errors;
-        if (query.error) delete query.error;
+  var query = deleteMessagesAndErrors(parseQuery(location.search));
+  var queryString = stringifyQuery(query);
 
-        return query;
-    };
-
-    var query = deleteMessagesAndErrors(parseQuery(location.search));
-    var queryString = stringifyQuery(query);
-
-    return history.pushState(null, '', location.pathname + queryString);
+  return history.pushState(null, '', location.pathname + queryString);
 })();
